dbc53b3c722f05068aa17f8cea6b8c7f4c0842b4
louis gueye <louis.gueye@gmail.com>
2013-04-03 23:38:36 +0200

    Implement pascal and balance

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,4 +1,5 @@
 package recfun
+
 import common._
 
 object Main {
@@ -14,15 +15,43 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == r || c == 0) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanceRecursive(accumulator: Int, chars: List[Char]): Boolean = chars match {
+      case head :: tail => {
+        val newAcc = head match {
+          case '(' => accumulator + 1
+          case ')' => accumulator - 1
+          case _ => accumulator
+        }
+        if (newAcc >= 0) balanceRecursive(newAcc, tail)
+        else false
+      }
+      case Nil => accumulator == 0
+    }
+    balanceRecursive(0, chars)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def countChangeRecursive(accumulator: Int, money: Int, coins: List[Int]): Int = coins match {
+      case head :: tail => {
+        val result: Int = money % head
+        if (result == money) countChangeRecursive(accumulator, money, tail)
+        else if (result == 0) countChangeRecursive(accumulator+1, money, tail)
+        else accumulator + countChangeRecursive(accumulator, result, tail)
+      }
+      case Nil => accumulator
+    }
+    countChangeRecursive(0, money, coins)
+  }
+
 }
