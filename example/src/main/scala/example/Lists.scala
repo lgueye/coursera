1d70baa813940def2712141ed112cc8047ae5add
louis gueye <louis.gueye@figarocms.fr>
2013-04-04 11:46:21 +0200

    implemented max and sum challenges

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -11,19 +11,22 @@ object Lists {
    * For this example assignment you can use the following methods in class
    * `List`:
    *
-   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
-   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
-   *    is empty an exception is thrown
-   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
-   *    list `xs` without its `head` element
+   * - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+   * - `xs.head: Int` returns the head element of the list `xs`. If the list
+   * is empty an exception is thrown
+   * - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+   * list `xs` without its `head` element
    *
-   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
-   *  solution.
+   * ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+   * solution.
    *
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = xs match {
+    case head :: tail => head + sum(tail)
+    case Nil => 0
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +41,12 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = xs match {
+    case head :: tail => {
+      if (tail.isEmpty) head
+      else
+        if (head > max(tail)) head else max(tail)
+    }
+    case Nil          =>  throw new NoSuchElementException
+  }
 }
