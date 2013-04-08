11e9ea997a50f738789ca0ad4d39361d6ee87c08
louis gueye <louis.gueye@figarocms.fr>
2013-04-08 15:31:49 +0200

    implement funsets solution

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -1,6 +1,5 @@
 package funsets
 
-import common._
 
 /**
  * 2. Purely Functional Sets.
@@ -20,30 +19,40 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): Set = ???
+  def singletonSet(elem: Int): Set = {
+    that: Int => (that == elem)
+  }
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = ???
+  def union(s: Set, t: Set): Set = {
+    elem: Int => contains(s, elem: Int) || contains(t, elem: Int)
+  }
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = ???
+  def intersect(s: Set, t: Set): Set = {
+    elem: Int => contains(s, elem: Int) && contains(t, elem: Int)
+  }
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = ???
+  def diff(s: Set, t: Set): Set = {
+    elem: Int => contains(s, elem: Int) && !contains(t, elem: Int)
+  }
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = ???
+  def filter(s: Set, p: Int => Boolean): Set = {
+    intersect(p, s)
+  }
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -55,23 +64,34 @@ object FunSets {
    */
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (!contains(s, a)) iter(a + 1)
+      else (p(a) && iter(a + 1))
     }
-    iter(???)
+    iter(-bound)
   }
 
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+    !forall(s, {
+      elem: Int => !p(elem)
+    })
+  }
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set = {
+    def iter(acc: Set, a: Int): Set = {
+      if (a > bound) acc
+      else if (!contains(s, a)) iter(acc, a + 1)
+      else iter(union(acc, singletonSet(f(a))), a+1)
+    }
+    iter({x: Int => false}, -bound)
+  }
 
   /**
    * Displays the contents of a set
@@ -87,4 +107,5 @@ object FunSets {
   def printSet(s: Set) {
     println(toString(s))
   }
+
 }
--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -8,12 +8,11 @@ import org.scalatest.junit.JUnitRunner
 /**
  * This class is a test suite for the methods in object FunSets. To run
  * the test suite, you can either:
- *  - run the "test" command in the SBT console
- *  - right-click the file in eclipse and chose "Run As" - "JUnit Test"
+ * - run the "test" command in the SBT console
+ * - right-click the file in eclipse and chose "Run As" - "JUnit Test"
  */
 @RunWith(classOf[JUnitRunner])
 class FunSetSuite extends FunSuite {
-
 
   /**
    * Link to the scaladoc - very clear and detailed tutorial of FunSuite
@@ -21,9 +20,9 @@ class FunSetSuite extends FunSuite {
    * http://doc.scalatest.org/1.9.1/index.html#org.scalatest.FunSuite
    *
    * Operators
-   *  - test
-   *  - ignore
-   *  - pending
+   * - test
+   * - ignore
+   * - pending
    */
 
   /**
@@ -47,30 +46,30 @@ class FunSetSuite extends FunSuite {
     assert(1 + 2 === 3)
   }
 
-  
+
   import FunSets._
 
   test("contains is implemented") {
     assert(contains(x => true, 100))
   }
-  
+
   /**
    * When writing tests, one would often like to re-use certain values for multiple
    * tests. For instance, we would like to create an Int-set and have multiple test
    * about it.
-   * 
+   *
    * Instead of copy-pasting the code for creating the set into every test, we can
    * store it in the test class using a val:
-   * 
-   *   val s1 = singletonSet(1)
-   * 
+   *
+   * val s1 = singletonSet(1)
+   *
    * However, what happens if the method "singletonSet" has a bug and crashes? Then
    * the test methods are not even executed, because creating an instance of the
    * test class fails!
-   * 
+   *
    * Therefore, we put the shared values into a separate trait (traits are like
    * abstract classes), and create an instance inside each test method.
-   * 
+   *
    */
 
   trait TestSets {
@@ -82,12 +81,12 @@ class FunSetSuite extends FunSuite {
   /**
    * This test is currently disabled (by using "ignore") because the method
    * "singletonSet" is not yet implemented and the test would fail.
-   * 
+   *
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
-    
+  test("singletonSet(1) contains 1") {
+
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
      * to the values "s1" to "s3". 
@@ -97,16 +96,97 @@ class FunSetSuite extends FunSuite {
        * The string argument of "assert" is a message that is printed in case
        * the test fails. This helps identifying which assertion failed.
        */
-      assert(contains(s1, 1), "Singleton")
+      assert(contains(s1, 1), "Singleton 1 should contain 1")
+      assert(contains(s2, 2), "Singleton 2 should contain 2")
+      assert(!contains(s2, 1), "Singleton 2 should not contain 1")
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements of both sets") {
     new TestSets {
       val s = union(s1, s2)
-      assert(contains(s, 1), "Union 1")
-      assert(contains(s, 2), "Union 2")
-      assert(!contains(s, 3), "Union 3")
+      assert(contains(s, 1), "Union should contain 1")
+      assert(contains(s, 2), "Union should contain 2")
+      assert(!contains(s, 3), "Union should not contain 3")
+    }
+  }
+
+  test("intersect contains elements that are in both sets") {
+    new TestSets {
+      val union1: FunSets.Set = union(singletonSet(1), singletonSet(2))
+      val union2: FunSets.Set = union(singletonSet(1), singletonSet(3))
+      val s = intersect(union1, union2)
+      assert(contains(s, 1), "Intersect should contain 1")
+      assert(!contains(s, 2), "Intersect should not contain 2")
+      assert(!contains(s, 3), "Intersect should not contain 3")
+    }
+  }
+
+  test("diff contains elements that are in the first set and not in the second set") {
+    new TestSets {
+      val union1: FunSets.Set = union(singletonSet(1), singletonSet(2))
+      val union2: FunSets.Set = singletonSet(1)
+      val s = diff(union1, union2)
+      assert(contains(s, 2), "Diff should contain 2")
+    }
+  }
+
+  test("filter contains elements of the the first set that match a predicate") {
+    new TestSets {
+      val union1: FunSets.Set = union(singletonSet(1), singletonSet(2))
+      val union2: FunSets.Set = union(union1, singletonSet(3))
+      val s = filter(union2, x => x > 1)
+      assert(contains(s, 2), "Filter should contain 2")
+      assert(contains(s, 3), "Filter should contain 3")
+    }
+  }
+
+  test("forall returns false if any element doesn't match a predicate") {
+    new TestSets {
+      val union1: FunSets.Set = union(singletonSet(1), singletonSet(2))
+      val union2: FunSets.Set = union(union1, singletonSet(3))
+      val s = forall(union2, x => x < 2)
+      assert(!s, "Forall should return false to (x < 2)")
+    }
+  }
+
+  test("forall returns true if all elements match a predicate") {
+    new TestSets {
+      val union1: FunSets.Set = union(singletonSet(1), singletonSet(2))
+      val union2: FunSets.Set = union(union1, singletonSet(3))
+      val s = forall(union2, x => x > 0)
+      assert(s, "Forall should return true to (x > 0)")
+    }
+  }
+
+  test("exists returns false if all elements don't match a predicate") {
+    new TestSets {
+      val union1: FunSets.Set = union(singletonSet(1), singletonSet(2))
+      val union2: FunSets.Set = union(union1, singletonSet(3))
+      val s = exists(union2, x => x < 0)
+      assert(!s, "Forall should return false to (x < 0)")
+    }
+  }
+
+  test("exists returns true if at least one element matches a predicate") {
+    new TestSets {
+      val union1: FunSets.Set = union(singletonSet(1), singletonSet(2))
+      val union2: FunSets.Set = union(union1, singletonSet(3))
+      val s = exists(union2, x => x > 2)
+      assert(s, "Forall should return true to (x > 2)")
+    }
+  }
+
+  test("map returns a subset of all element transformed by f") {
+    new TestSets {
+      val union1: FunSets.Set = union(singletonSet(1), singletonSet(2))
+      val union2: FunSets.Set = union(union1, singletonSet(3))
+      val s = map(union2, x => x * 2)
+      assert(contains(s, 2), "map should contain 2 for element 1")
+      assert(contains(s, 4), "map should contain 4 for element 2")
+      assert(!contains(s, 0), "map should not return 0 for element 0")
+      assert(!contains(s, 8), "map should not return 8 for element 4")
+      assert(contains(s, 6), "map should contain 6 for element 3")
     }
   }
 }
